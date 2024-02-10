7f3b46a2a10b8a400a853a9c4bdfcf27d6528260
Iain Taylor <iain.taylor.94@gmail.com>
2024-02-10 15:50:37 +0000

    completed get permutation

--- main.c
+++ main.c
@@ -14,6 +14,18 @@ void usage (char *);
 const int kMaxStringLength = 1000;
 bool getStrings (char [kMaxStringLength], char [kMaxStringLength]);
 
+// Common Permutation
+const int kNumLettersInAlphabet = 26;
+int gStr1Chars [kNumLettersInAlphabet] = { 0 };
+int gStr2Chars [kNumLettersInAlphabet] = { 0 };
+char gPermutation [kMaxStringLength];
+int numChars (char *, char);
+void getNumChars (char *, char *);
+void printNumChars (int *, int *);
+void getPermutation (int *, int *);
+void printPermutation (char *);
+
+
 int main(int argc, char *argv[]) {
   /*-------------------------------FILE INITIALIZATION START-----------------------------*/
   if (argc != 2) usage (argv[0]);
@@ -32,6 +44,12 @@ int main(int argc, char *argv[]) {
   
   while (getStrings(str1, str2)) {
     printf ("str1 : %s\nstr2 : %s\n\n", str1, str2);
+
+    getNumChars(str1, str2);
+    printNumChars(gStr1Chars, gStr2Chars);
+
+    getPermutation(gStr1Chars, gStr2Chars);
+    printPermutation (gPermutation);
   }
 
 
@@ -61,4 +79,48 @@ bool getStrings (char str1 [kMaxStringLength], char str2 [kMaxStringLength]) {
   str1[strcspn(str1, "\n")] = 0;
   str2[strcspn(str2, "\n")] = 0;
   return true;
+}
+
+// Common Permutation
+int numChars (char *str, char c) {
+  int num = 0;
+  for (int i = 0; *(str + i) != '\0'; i++) {
+    if (*(str + i) == c) num++;
+  }
+  return num;
+}
+void getNumChars (char *str1, char *str2) {
+  for (int i = 0; i < kNumLettersInAlphabet; i++) {
+    gStr1Chars[i] = numChars(str1, 'a' + i);
+    gStr2Chars[i] = numChars(str2, 'a' + i);
+  }
+}
+void printNumChars (int *str1, int *str2) {
+  printf ("str1:     str2:\n");
+  printf ("char num  char num\n");
+  printf ("---- ---  ---- ---\n");
+  for (int i = 0; i < kNumLettersInAlphabet; i++) {
+    printf ("%4c %3d", 'a' + i, str1[i]);
+    printf ("  ");
+    printf ("%4c %3d\n", 'a' + i, str2[i]);
+  }
+  printf ("\n");
+}
+void getPermutation (int *str1, int *str2) {
+  int permutationIndex = 0;
+  for (int i = 0; i < kNumLettersInAlphabet; i++) {
+    // Get min
+    int min = INT_MAX;
+    if (str1[i] < str2[i]) min = str1[i];
+    else if (str1[i] > str2[i]) min = str2[i]; 
+    else min = str1[i];
+
+    for (; min > 0; min--) {
+      gPermutation [permutationIndex] = 'a' + i;
+      permutationIndex++;
+    }
+  }
+}
+void printPermutation (char *permutation) {
+  printf ("permutation: %s\n", permutation);
 }
